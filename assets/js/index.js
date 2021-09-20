7fac2df5040a87806402344135476def0349e4dd
NOREEN-WB <nwbutt@hotmail.com>
2021-09-20 18:04:15 +0100

    categories Array

---
+++ assets/js/index.js
@@ -0,0 +1,15 @@
+// Type Array to differentiate menu
+const type = [
+  {
+    id: 1,
+    name: "Traditional",
+  },
+  {
+    id: 2,
+    name: "Chienese",
+  },
+  {
+    id: 3,
+    name: "Continental",
+  },
+];
