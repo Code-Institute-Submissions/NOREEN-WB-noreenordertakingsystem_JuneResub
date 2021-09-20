90436aa6246221dacf66ffed37ecd9b01179d8d4
NOREEN-WB <nwbutt@hotmail.com>
2021-09-20 18:15:37 +0100

    menu array

--- assets/js/index.js
+++ assets/js/index.js
@@ -13,3 +13,120 @@ const type = [
     name: "Continental",
   },
 ];
+// Menu Array to create menu
+const menus = [
+  {
+    id: 1,
+    typeId: 1,
+    name: "karahi",
+    price: 500,
+  },
+  {
+    id: 2,
+    typeId: 1,
+    name: "Biryani",
+    price: 150,
+  },
+  {
+    id: 3,
+    typeId: 1,
+    name: "Pulao",
+    price: 130,
+  },
+  {
+    id: 4,
+    typeId: 1,
+    name: "Nihari",
+    price: 190,
+  },
+  {
+    id: 5,
+    typeId: 1,
+    name: "Haleem",
+    price: 70,
+  },
+  {
+    id: 6,
+    typeId: 1,
+    name: "Halwa Puri",
+    price: 50,
+  },
+  {
+    id: 7,
+    typeId: 1,
+    name: "Mutton Korma",
+    price: 200,
+  },
+  {
+    id: 101,
+    typeId: 2,
+    name: "chinnese Rice",
+    price: 100,
+  },
+  {
+    id: 102,
+    typeId: 2,
+    name: "vegitable Rice",
+    price: 90,
+  },
+  {
+    id: 103,
+    typeId: 2,
+    name: "Dumpling",
+    price: 75,
+  },
+  {
+    id: 104,
+    typeId: 2,
+    name: "Oyster",
+    price: 200,
+  },
+  {
+    id: 105,
+    typeId: 2,
+    name: "Prawn",
+    price: 95,
+  },
+  {
+    id: 106,
+    typeId: 2,
+    name: "Shrimps",
+    price: 110,
+  },
+  {
+    id: 201,
+    typeId: 3,
+    name: "Beef Burger",
+    price: 80,
+  },
+  {
+    id: 202,
+    typeId: 3,
+    name: "Chicken Burger",
+    price: 60,
+  },
+  {
+    id: 203,
+    typeId: 3,
+    name: "Grill Burger",
+    price: 70,
+  },
+  {
+    id: 204,
+    typeId: 3,
+    name: "BBQ-Pizza",
+    price: 70,
+  },
+  {
+    id: 205,
+    typeId: 3,
+    name: "Chicken Tikka Pizza",
+    price: 70,
+  },
+  {
+    id: 206,
+    typeId: 3,
+    name: "Jalapeño Loaded Pizza",
+    price: 70,
+  },
+];
